c66fda9b78be28ff928a5f7c056040bcebf5e134
Orlando <orlandocrawford@hotmail.com>
2020-10-21 17:36:20 +0100

    bump

---
+++ build/precache-manifest.396da95d0cdf1bce2dc2448081044848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4926d09d6691efb38b43e4b77af4e012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a549c4136aa8bfcce6",
+    "url": "/static/css/main.036cfc6b.chunk.css"
+  },
+  {
+    "revision": "16fe6614211e586e1526",
+    "url": "/static/js/2.3237a1a6.chunk.js"
+  },
+  {
+    "revision": "86e52ca2e9503aa3f641d2c68029f28e",
+    "url": "/static/js/2.3237a1a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a549c4136aa8bfcce6",
+    "url": "/static/js/main.2e0ef133.chunk.js"
+  },
+  {
+    "revision": "bf8f8bc46ba993ef14efaf028a5b8f19",
+    "url": "/static/js/main.2e0ef133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
